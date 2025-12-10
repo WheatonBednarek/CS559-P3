b58ad4c2fba04b4e5d8553c6a214d51125e27862
WheatonBednarek <wheatonbednarek@gmail.com>
2025-12-09 22:04:32 -0600

    another github pages fix

--- CameraControls.js
+++ CameraControls.js
@@ -1,5 +1,5 @@
 import * as THREE from 'three';
-import { OrbitControls } from '/node_modules/three/examples/jsm/controls/OrbitControls.js';
+import { OrbitControls } from './node_modules/three/examples/jsm/controls/OrbitControls.js';
 import { WorldObject } from './WorldObject.js';
 export class CameraControls extends WorldObject {
 	constructor(camera, renderer) {
